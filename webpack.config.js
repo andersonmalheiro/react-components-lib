e87a6791e4b86e95e36d944fdde9451c53221df0
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2022-02-02 17:15:43 -0300

    Adding storybook and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const config = {
+  target: "web",
+  entry: {
+    index: "./src/index.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index.js",
+    library: "ReactComponentsLib",
+    libraryTarget: "umd",
+    globalObject: "this",
+    umdNamedDefine: true,
+  },
+  watchOptions: {
+    aggregateTimeout: 600,
+    ignored: /node_modules/,
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+      cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, "./dist")],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: [/node_modules/, /test/, /__tests__/],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "postcss-loader",
+          {
+            loader: "sass-loader",
+            options: { implementation: require("sass") },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === "development") {
+    // * add some development rules here
+  } else if (argv.mode === "production") {
+    // * add some prod rules here
+  } else {
+    throw new Error("Specify env");
+  }
+
+  return config;
+};
